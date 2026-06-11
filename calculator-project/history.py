b8b83fa38b894a8ff683bc06827c3b03f24ac524
Yacho-glitch <yahyabousksou@gmail.com>
2026-06-11 18:14:06 +0100

    feat: add load history function to read txt file content

--- calculator-project/history.py
+++ calculator-project/history.py
@@ -9,3 +9,18 @@ def save_history(history_list):
     file.close()
 
 
+def load_history():
+    try:
+        file = open('history', 'r')
+        lines = file.readlines() # Read all lines
+        file.close()
+
+        clean_list = []
+        for line in file:
+            clean_line = line.strip()
+            clean_list.append(clean_list)
+
+        return clean_list
+     
+    except FileNotFoundError:
+        return []

027dabb617fdcbe8de238f136f2eda6fd8a7460e
Yacho-glitch <yahyabousksou@gmail.com>
2026-06-11 17:54:56 +0100

    feat: add save history function to save history in file txt

--- calculator-project/history.py
+++ calculator-project/history.py
@@ -1,2 +1,11 @@
 # This file saves calculation history
 
+def save_history(history_list):
+    file = open('history.txt', 'w')
+
+    for item in history_list:
+        file.write(item + "\n")
+
+    file.close()
+
+

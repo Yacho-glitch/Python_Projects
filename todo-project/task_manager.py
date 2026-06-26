5e3f8440a764e6f3f84775c2c3fd6e41dcd0308c
Yacho-glitch <yahyabousksou@gmail.com>
2026-06-26 17:13:08 +0100

    feat: add show_all_tasks method

--- todo-project/task_manager.py
+++ todo-project/task_manager.py
@@ -8,10 +8,24 @@ class TaskManager:
         new_task = Task(title, date)
         self.tasks.append(new_task)
 
+    def show_all_tasks(self):
+         if (self.tasks) == 0:
+             print("No tasks yet!")
+             return
+         else:
+             print("-----History📝-----")
+             for index, task in enumerate(self.tasks, start=1):
+                 print(f"{index}. {task.show()}")
+                 
+
 if __name__ == "__main__":
     manager = TaskManager()
+    manager.show_all_tasks()
+
     manager.add_task("Learn Java", "2027-18-03")
     manager.add_task("Learn French", "2023-05-05")
 
-    print(f"Number of tasks: {len(manager.tasks)}")
-    print(manager.tasks[0].show())
+    manager.show_all_tasks()
+
+    # print(f"Number of tasks: {len(manager.tasks)}")
+    # print(manager.tasks[0].show())
